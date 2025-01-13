aab26b6ec5a62c41d18ccce832468cb31cf38ab7
priyanshishikha <priyanshi.shikha@opsmx.io>
2025-01-13 13:55:51 +0530

    error msg imporved

--- november2024december2024/featureNode.go
+++ november2024december2024/featureNode.go
@@ -35,8 +35,7 @@ func migrateFeatureToConfigKeyValues(gqlClient graphql.Client) error {
 	}
 
 	if len(res.QueryFeatureMode) == 0 {
-		logger.Sl.Debugf("No record for feature mode")
-		return nil
+		return fmt.Errorf("ro record for feature mode")
 	}
 
 	detailsToMigrate := make(map[string]FeatureModeDetails)
